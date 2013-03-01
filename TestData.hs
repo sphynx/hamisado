2908ff23d8da2052cddf8cf71949c9e4aa6ab920
Ivan N. Veselov <veselov@gmail.com>
2013-03-01 16:30:16 +0200

    Added experiments description and timings

--- TestData.hs
+++ TestData.hs
@@ -37,3 +37,37 @@ losingMoves5 =
   , Move (8,1) (8,6)
   , Move (8,1) (8,7)
   ]
+
+{-
+
+== Experiments and losing first moves found so far. ==
+
+Params: negascout, d=7, moves sorted
+Moves:  9: [A1-A7,B1-H7,D1-D7,E1-E7,G1-A7,H1-H7,A1-A6,H1-H6,C1-A3]
+Time:   2.155s
+
+Params: alphabeta, d=7, moves sorted
+Moves:  9: [A1-A7,B1-H7,D1-D7,E1-E7,G1-A7,H1-H7,A1-A6,H1-H6,C1-A3]
+Time:   2.52s
+
+Params: negascout, d=9, moves sorted
+Result: 9: [A1-A7,B1-H7,D1-D7,E1-E7,G1-A7,H1-H7,A1-A6,H1-H6,C1-A3]
+Time:   17.36s
+
+Params: alphabeta, d=9, moves sorted
+Result: 9: [A1-A7,B1-H7,D1-D7,E1-E7,G1-A7,H1-H7,A1-A6,H1-H6,C1-A3]
+Time:   19.83s
+
+Params: negascout, d=11, moves sorted
+Result: 10: [A1-A7,B1-H7,D1-D7,E1-E7,G1-A7,H1-H7,A1-A6,H1-H6,C1-A3,A1-B2]
+Time:   2m8.022s
+
+Params: alphabeta, d=11, moves sorted
+Result: 10: [A1-A7,B1-H7,D1-D7,E1-E7,G1-A7,H1-H7,A1-A6,H1-H6,C1-A3,A1-B2]
+Time:   2m34.016s
+
+Params: negascout, d=13, moves sorted
+Moves:  23: [A1-A7,B1-H7,D1-D7,E1-E7,G1-A7,H1-H7,A1-A6,H1-H6,E1-A5,F1-B5,A1-D4,A1-C3,C1-A3,F1-D3,G1-E3,H1-F3,A1-B2,B1-A2,B1-C2,D1-C2,E1-F2,F1-G2,G1-F2]
+Time:   17m28.199s
+
+-}
